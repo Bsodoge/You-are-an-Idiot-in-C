b98c400c0cc33cf1009de07d6e309dd71c213770
bsodoge <kashhh4u@gmail.com>
2024-07-18 14:45:20 +0100

    Store window in struct

--- main.c
+++ main.c
@@ -4,52 +4,59 @@
 //Build with gcc `pkg-config --cflags --libs gtk+-2.0` main.c -o gtktest
 
 struct Vector{
-    int x;
-    int y;
+    gint x;
+    gint y;
 } typedef vector;
 
-vector position = { 0 , 0 };
-vector velocity = { -1 , 1 };
-int isWhite = 0;
+struct Window{
+    GtkWindow *win;
+    GtkImage *img;
+    vector position;
+    vector velocity;
+    gint isWhite;
+} typedef window;
 
-void set_inital_pos(){
+/* void set_inital_pos(){
     GdkScreen *screen = gdk_screen_get_default();
     gint width = gdk_screen_get_width(screen);
     gint height = gdk_screen_get_height(screen);
     position.x = width/2;
     position.y = height/2;
-} 
+}  */
 
-gint change_image(gpointer ptr){
-    gtk_image_clear(ptr);
-    if(isWhite == 0){
-        gtk_image_set_from_file(ptr, "white.svg");
-        isWhite = 1;
+gint change_image(gpointer currentWindow){
+    window *ptr = currentWindow;
+    gtk_image_clear(ptr->img);
+    if(ptr->isWhite == 0){
+        gtk_image_set_from_file(ptr->img, "white.svg");
+        ptr->isWhite = 1;
     } else {
-        gtk_image_set_from_file(ptr, "black.svg");
-        isWhite = 0;
+        gtk_image_set_from_file(ptr->img, "black.svg");
+        ptr->isWhite = 0;
     }
     return 1;
 }
 
-void detectEdge(gpointer ptr){
+void detectEdge(gpointer currentWindow){
     gint currentWidth, currentHeight;
+    window *ptr = currentWindow;
     GdkScreen *screen = gdk_screen_get_default();
     gint width = gdk_screen_get_width(screen);
     gint height = gdk_screen_get_height(screen);
-    gtk_window_get_size(ptr, &currentWidth, &currentHeight);
-    if((position.x > width - currentWidth) ||  (position.x < 0)){
-        velocity.x = -velocity.x;
+    gtk_window_get_size(ptr->win, &currentWidth, &currentHeight);
+    if((ptr->position.x > width - currentWidth) ||  (ptr->position.x < 0)){
+        ptr->velocity.x = -ptr->velocity.x;
     }
-    if((position.y > height - currentHeight) || (position.y < 0)){
-        velocity.y = -velocity.y;
+    if((ptr->position.y > height - currentHeight) || (ptr->position.y < 0)){
+        ptr->velocity.y = -ptr->velocity.y;
     }
 }
 
-gint move_window(gpointer ptr){
-    position.x += velocity.x;
-    position.y += velocity.y;
-    gtk_window_move(ptr,  position.x, position.y);
+gint move_window(gpointer currentWindow){
+    window *ptr = currentWindow;
+    ptr->position.x += ptr->velocity.x;
+    ptr->position.y += ptr->velocity.y;
+    gtk_window_move(ptr->win,  ptr->position.x, ptr->position.y);
     detectEdge(ptr);
     return 1;
 }
@@ -58,9 +65,26 @@ void end_program(GtkWidget *wid, gpointer ptr){
     gtk_main_quit();
 }
 
-void on_key_press (GtkWidget *widget, GdkEventKey *event, gpointer user_data){ 
+/* void procreate(){
+    for (gint i = 0; i < 1; i++){
+        GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        GtkWidget *img = gtk_image_new_from_file("black.svg");
+        gtk_container_add(GTK_CONTAINER(win), img);
+        gtk_widget_show_all(win);
+        g_timeout_add(1, move_window, win);
+        g_timeout_add(400, change_image, img);
+    }
+} */
+
+void on_key_press (GtkWidget *widget, GdkEventKey *event, gpointer ptr){ 
     if(event->keyval == 65513 || event->keyval == 65514 || event->keyval == 65428 || event->keyval == 65507 || event->keyval == 65508 || event->keyval == 65535 || event->keyval == 65473){
-        gtk_main_quit();
+/*         procreate(); */
+        GtkWidget *dlg = gtk_dialog_new_with_buttons("Warning", GTK_WINDOW(ptr), GTK_DIALOG_DESTROY_WITH_PARENT, "Ok", 0, NULL);
+        GtkWidget *lbl = gtk_label_new("You are an Idiot!");
+        gtk_container_add(GTK_CONTAINER(gtk_dialog_get_content_area(GTK_DIALOG(dlg))), lbl);
+        gtk_widget_show_all(dlg);
+        gtk_dialog_run(GTK_DIALOG(dlg));
+        gtk_widget_destroy(dlg);
     }
 }
 
@@ -69,11 +93,12 @@ int main(int argc, char *argv[]){
 /*     set_inital_pos(); */
     GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     GtkWidget *img = gtk_image_new_from_file("black.svg");
-    g_signal_connect(win, "key_press_event" ,G_CALLBACK(on_key_press), NULL);
+    g_signal_connect(win, "key_press_event" ,G_CALLBACK(on_key_press), win);
     gtk_container_add(GTK_CONTAINER(win), img);
     gtk_widget_show_all(win);
-    g_timeout_add(1, move_window, win);
-    g_timeout_add(400, change_image, img);
+    window newWindow = {GTK_WINDOW(win), GTK_IMAGE(img), {0, 0}, {1, 1}, 0}; 
+    g_timeout_add(1, move_window, &newWindow);
+    g_timeout_add(400, change_image, &newWindow);
     gtk_main();
     return 0;
 }
